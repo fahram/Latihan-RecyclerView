ad6a21ad142b325bc46cce7dfe74f9c2d010d5a8
Khaidir Fahram <khaidir@fahram.com>
2022-03-04 10:32:51 +0700

    Create DistroAdapter.kt

---
+++ app/src/main/java/com/fahram/latihanrecyclerview/DistroAdapter.kt
@@ -0,0 +1,26 @@
+package com.fahram.latihanrecyclerview
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DistroAdapter(private val listDistro: ArrayList<Distro>) :
+    RecyclerView.Adapter<DistroAdapter.ListViewHolder>() {
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
