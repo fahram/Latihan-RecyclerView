b51c82f89820900759cc37d81b6ee224355c9dea
Khaidir Fahram <khaidir@fahram.com>
2022-03-04 10:41:20 +0700

    handle button click

--- app/src/main/java/com/fahram/latihanrecyclerview/DistroAdapter.kt
+++ app/src/main/java/com/fahram/latihanrecyclerview/DistroAdapter.kt
@@ -3,8 +3,10 @@ package com.fahram.latihanrecyclerview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class DistroAdapter(private val listDistro: ArrayList<Distro>,
@@ -16,6 +18,7 @@ class DistroAdapter(private val listDistro: ArrayList<Distro>,
         var tvName: TextView = itemView.findViewById(R.id.tv_name)
         var tvOrigin: TextView = itemView.findViewById(R.id.tv_origin)
         var tvBase: TextView = itemView.findViewById(R.id.tv_base)
+        var btnFav: Button = itemView.findViewById(R.id.btn_favorite)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -32,6 +35,9 @@ class DistroAdapter(private val listDistro: ArrayList<Distro>,
         holder.ivLogo.setImageResource(logo)
         holder.itemView.setOnClickListener {
             onItemClick(listDistro[position])
+        }
+        holder.btnFav.setOnClickListener {
+            Toast.makeText(holder.itemView.context, "$name added to Favorite distros", Toast.LENGTH_SHORT).show()
         }
     }
 
